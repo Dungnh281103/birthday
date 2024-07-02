b8b6cd50e9cd25434dfd97d41243c3af2032d8be
Dungnh281103 <dunghoangnguyen82@gmail.com>
2024-07-02 18:56:22 +0700

    oke

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'Dũng',
+        age: 18,
+        image:'https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/448482096_2194350870905870_739353467904001140_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG5iELZpIcZ_8NVQOmnFTtrrdj1dshzvXGt2PV2yHO9cRmlaWjzzBJUODVeHQHwBotghhUNZEyYiipwYu1qvBAk&_nc_ohc=lSaiYAJ1R0sQ7kNvgG2jEGA&_nc_ht=scontent.fhan14-3.fna&oh=00_AYD8d4AiBqhzUXRqTAh9Z8AZBWJwLvz68olcAOWl-Y-B4A&oe=66899C07',
+
+    },
+    {
+        id: 2,
+        name: 'Dũng',
+        age: 19,
+        image:'https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/448482096_2194350870905870_739353467904001140_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG5iELZpIcZ_8NVQOmnFTtrrdj1dshzvXGt2PV2yHO9cRmlaWjzzBJUODVeHQHwBotghhUNZEyYiipwYu1qvBAk&_nc_ohc=lSaiYAJ1R0sQ7kNvgG2jEGA&_nc_ht=scontent.fhan14-3.fna&oh=00_AYD8d4AiBqhzUXRqTAh9Z8AZBWJwLvz68olcAOWl-Y-B4A&oe=66899C07',
+
+    },
+    {
+        id: 3,
+        name: 'Dũng',
+        age: 10,
+        image:'https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/448482096_2194350870905870_739353467904001140_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG5iELZpIcZ_8NVQOmnFTtrrdj1dshzvXGt2PV2yHO9cRmlaWjzzBJUODVeHQHwBotghhUNZEyYiipwYu1qvBAk&_nc_ohc=lSaiYAJ1R0sQ7kNvgG2jEGA&_nc_ht=scontent.fhan14-3.fna&oh=00_AYD8d4AiBqhzUXRqTAh9Z8AZBWJwLvz68olcAOWl-Y-B4A&oe=66899C07',
+
+    },
+    {
+        id: 4,
+        name: 'Dũng',
+        age: 21,
+        image:'https://scontent.fhan14-3.fna.fbcdn.net/v/t39.30808-6/448482096_2194350870905870_739353467904001140_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG5iELZpIcZ_8NVQOmnFTtrrdj1dshzvXGt2PV2yHO9cRmlaWjzzBJUODVeHQHwBotghhUNZEyYiipwYu1qvBAk&_nc_ohc=lSaiYAJ1R0sQ7kNvgG2jEGA&_nc_ht=scontent.fhan14-3.fna&oh=00_AYD8d4AiBqhzUXRqTAh9Z8AZBWJwLvz68olcAOWl-Y-B4A&oe=66899C07',
+
+    },
+];
